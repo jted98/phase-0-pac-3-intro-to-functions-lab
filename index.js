064e6cc392a7c9b3145a3cfc31f40fac6f5f4682
Jeffrey Tedford <mike.tedford12@gmail.com>
2023-01-08 10:10:28 -0600

    commit tree 6867484e5fc813b4190cdf06b78d9723c0ab5534

---
+++ index.js
@@ -0,0 +1,10 @@
+"Hello!".toUpperCase(); // 'HELLO!'
+function shout(string) {
+    return string.toUpperCase ()
+}
+"Hello!".toLowerCase(); // 'hello!'
+function whisper(string) {
+    return string.toLowerCase ()
+}
+function logShout(string).toUpperCase
+
